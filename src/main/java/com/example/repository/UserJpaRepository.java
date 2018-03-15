ea512f9b64b61a89bc47ddb7a56f2e8c1d595b22
IBOXPAY\xuxiongwei <xuxiongwei@iboxpay.com>
2018-03-15 17:09:48 +0800

    spring boot style crud

--- src/main/java/com/example/repository/UserJpaRepository.java
+++ src/main/java/com/example/repository/UserJpaRepository.java
@@ -1,4 +1,7 @@
 package com.example.repository;
 
-public interface UserJpaRepository {
+import com.example.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserJpaRepository extends JpaRepository<User, Long> {
 }
