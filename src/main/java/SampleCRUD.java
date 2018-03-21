64569471a697e002340eca43afc0bd789a0360d5
IBOXPAY\xuxiongwei <xuxiongwei@iboxpay.com>
2018-03-21 13:45:05 +0800

    启用多线程

--- src/main/java/SampleCRUD.java
+++ src/main/java/SampleCRUD.java
@@ -1,45 +1,86 @@
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.dbutils.*;
 import org.apache.commons.dbcp.BasicDataSource;
 
-public class SampleCRUD {
-    // 使用DBUtils的QueryRunner测试sql插入性能（内部使用PreparedStatement预处理）
-    //insert 50000 | 10000 BatchSize  用时 785(ms)
-    //insert 50000 | 100 BatchSize  用时 1898(ms)
-    //insert 50000 | 1 BatchSize  用时 147176(ms)
-    public static void main(String args[]) {
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/test_my?serverTimezone=UTC&useSSL=false&rewriteBatchedStatements=true");
-        dataSource.setUsername("someoneCool");
-        dataSource.setPassword("123456");
-        QueryRunner run = new QueryRunner(dataSource);
+public class SampleCRUD implements Runnable {
 
+    private final static int StepSize = 20000; //使用固定步长避免id冲突
+    private final static int threadNums = 100;
+    private static int alreadyDoneRecord = 122000000;
+
+    private int start_id;
+
+    private SampleCRUD(int start_id) {
+        this.start_id = start_id;
+    }
+
+    private static void startInsert() throws InterruptedException {
+        long startTime = System.currentTimeMillis();
+        ArrayBlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<Runnable>(10);
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(100, 500, 1, TimeUnit.MINUTES, workQueue);
+        for (int i = 0; i < threadNums; i++) {
+            SampleCRUD thread = new SampleCRUD(i * SampleCRUD.StepSize);
+            pool.execute(thread);
+        }
+        pool.shutdown();
+        while (true) {
+            if (pool.isTerminated()) {
+                break;
+            }
+            Thread.sleep(100);
+        }
+        System.out.println("insert " + (alreadyDoneRecord + StepSize * threadNums) + " ~ " + (alreadyDoneRecord + 2 * StepSize * threadNums) + "(不含)  | " + StepSize + " BatchSize " + " 用时 " + (System.currentTimeMillis() - startTime) + "(ms)");
+    }
+
+    public void run() {
         try {
-            int totalData = 50000;
-            int times = 5;
-            do {
-                int linePerBatch = totalData / times;
-                Object[][] objArray = new Object[linePerBatch][];
-                for (int i = 0; i < linePerBatch; i++) {
-                    objArray[i] = new Object[]{null, "xxw", "123", 11};
-                }
+            BasicDataSource dataSource = new BasicDataSource();
+            dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+            dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/ibox_trainee?serverTimezone=UTC&useSSL=false&rewriteBatchedStatements=true");
+            dataSource.setUsername("someoneCool");
+            dataSource.setPassword("123456");
 
-                long startTime = System.currentTimeMillis();
-                for (int i = 0; i < times; i++) {
-                    run.insertBatch("insert into test_my.user values (?, ?, ?, ?)", new ResultSetHandler<Object[]>() {
-                        public Object[] handle(ResultSet rs) {
-                            return null;
-                        }
-                    }, objArray);
-                }
-                System.out.println("insert " + totalData + " | " + linePerBatch + " BatchSize " + " 用时 " + (System.currentTimeMillis() - startTime) + "(ms)");
-                times *= 100;
-            } while (times <= totalData);
+            StringBuilder sb = new StringBuilder("insert into ibox_trainee.shopper values ");
+            for (int i = 0; i < StepSize; i++) {
+                sb.append("(").append(alreadyDoneRecord + start_id + i).append(", '")
+                        .append(UUID.randomUUID().toString().replace("-", "")).append("', '")
+                        .append(("" + System.currentTimeMillis()).substring(6))
+                        .append(new Random().nextInt(10000)).append("', ")
+                        .append("'0', ")
+                        .append("'1304924151@qq.com', ")
+                        .append("'123456'),");
+            }
 
+            new QueryRunner(dataSource).insert(sb.substring(0, sb.length() - 1) + ";", new ResultSetHandler<Object[]>() {
+                public Object[] handle(ResultSet rs) {
+                    return null;
+                }
+            });
         } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String args[]) {
+        try {
+            long startTime = System.currentTimeMillis();
+            int times = 10;
+            int stepPerLoop = SampleCRUD.StepSize * SampleCRUD.threadNums;
+            int index = 0;
+            while (index <= times) {
+                index++;
+                SampleCRUD.startInsert();
+                SampleCRUD.alreadyDoneRecord += stepPerLoop;
+            }
+            System.out.println("总共插入 " + stepPerLoop * times + " 用时 " + (System.currentTimeMillis() - startTime) + "(ms)");
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
