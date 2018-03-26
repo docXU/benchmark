c3b0bce743217fd11bffdebd6c25bc043d0beb75
IBOXPAY\xuxiongwei <xuxiongwei@iboxpay.com>
2018-03-26 18:02:50 +0800

    试用Restful注解

--- consumer-do/src/main/java/runner/App.java
+++ consumer-do/src/main/java/runner/App.java
@@ -12,6 +12,6 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(App.class, args);
+        SpringApplication.run(App.class, args);
     }
 }
