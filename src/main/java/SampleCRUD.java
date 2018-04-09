9aee07228d7aa83c369ef545d85012d628e2048d
IBOXPAY\xuxiongwei <xuxiongwei@iboxpay.com>
2018-04-09 17:46:45 +0800

    插入订单数据1000w+

--- src/main/java/SampleCRUD.java
+++ src/main/java/SampleCRUD.java
@@ -3,13 +3,13 @@ import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
-import java.util.UUID;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.dbutils.*;
 import org.apache.commons.dbcp.BasicDataSource;
+import util.DateUtil;
 
 /**
  * @author xuxiongwei
@@ -18,29 +18,28 @@ public class SampleCRUD implements Runnable {
     /**
      * 使用固定步长避免id冲突
      */
-    private final static int STEP_SIZE = 20000;
-    private final static int THREAD_NUM = 100;
-    private static int alreadyDoneRecord = 6380010;
+    private final static int STEP_SIZE = 50000;
+    private final static int THREAD_NUM = 20;
+    private static int alreadyDoneRecord = 9212046;
 
-    private int start_id;
+    private int startId;
 
-    private SampleCRUD(int start_id) {
-        this.start_id = start_id;
+    private SampleCRUD(int startId) {
+        this.startId = startId;
     }
 
     private static void startInsert() throws InterruptedException {
 
         ArrayBlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<Runnable>(50);
-        ThreadPoolExecutor pool = new ThreadPoolExecutor(50, 101, 1, TimeUnit.MINUTES, workQueue);
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(50, 100, 1, TimeUnit.MINUTES, workQueue);
         for (int i = 0; i < THREAD_NUM; i++) {
             SampleCRUD thread = new SampleCRUD(alreadyDoneRecord + i * SampleCRUD.STEP_SIZE);
             pool.execute(thread);
         }
         pool.shutdown();
         while (!pool.isTerminated()) {
-            Thread.sleep(100);
+            Thread.sleep(500);
         }
-
     }
 
     public void run() {
@@ -51,19 +50,29 @@ public class SampleCRUD implements Runnable {
             dataSource.setUsername("someoneCool");
             dataSource.setPassword("123456");
 
-            StringBuilder sb = new StringBuilder("insert into ibox_trainee.business values ");
+            StringBuilder sb = new StringBuilder("insert into ibox_trainee.tb_order values ");
             Random random = new Random();
             SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
             for (int i = 0; i < STEP_SIZE; i++) {
-                sb.append("(").append(start_id + i).append(", '")
-                        .append(UUID.randomUUID().toString()).append("', ")
-                        .append("'深圳" + random.nextInt(10000) + "', ")
-                        .append("'13049474755'").append(", ")
-                        .append("0, ")
-                        .append("null, ")
-                        .append("'" + sd.format(new Date()) + "', ")
-                        .append("0, ")
-                        .append("101),");
+                //写入订单表，随机匹配商户和顾客id
+                //随机写入下单时间
+                Date randomDate = DateUtil.randomDate("2016-01-01 00:00:00", "2018-12-31 23:59:59");
+                try {
+                    sb.append("(")
+                            .append(startId + i).append(", ")
+                            .append(random.nextInt(10)).append(", ")
+                            .append(random.nextInt(100)).append(", ")
+                            .append("0").append(", ")
+                            .append("'").append(sd.format(randomDate)).append("', ")
+                            .append("0, ")
+                            .append(random.nextInt(10000) + 1.52).append(", ")
+                            .append("0, ")
+                            .append("null")
+                            .append("),");
+                } catch (Exception e) {
+                    System.out.println("构造sql集出错，继续构造下一条sql");
+                    System.out.println(e.getMessage());
+                }
             }
             long startTime = System.currentTimeMillis();
             new QueryRunner(dataSource).insert(sb.substring(0, sb.length() - 1) + ";", new ResultSetHandler<Object[]>() {
@@ -74,7 +83,7 @@ public class SampleCRUD implements Runnable {
             synchronized (SampleCRUD.class) {
                 alreadyDoneRecord += STEP_SIZE;
             }
-            System.out.println("insert " + (start_id) + " ~ " + (start_id + STEP_SIZE) + "(不含)  | " + STEP_SIZE + " BatchSize " + " 用时 " + (System.currentTimeMillis() - startTime) + "(ms)");
+            System.out.println("insert " + (startId) + " ~ " + (startId + STEP_SIZE) + "(不含)  | " + STEP_SIZE + " BatchSize " + " 用时 " + (System.currentTimeMillis() - startTime) + "(ms)");
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (Exception e) {
