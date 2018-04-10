14fcda2c09ce330558112ee8313746fb06792726
IBOXPAY\xuxiongwei <xuxiongwei@iboxpay.com>
2018-04-10 10:15:29 +0800

    连续插入2000w条订单数据（平均分散在1000户商家）

--- src/main/java/SampleCRUD.java
+++ src/main/java/SampleCRUD.java
@@ -18,28 +18,15 @@ public class SampleCRUD implements Runnable {
     /**
      * 使用固定步长避免id冲突
      */
-    private final static int STEP_SIZE = 50000;
-    private final static int THREAD_NUM = 20;
-    private static int alreadyDoneRecord = 9212046;
+    private int STEP_SIZE;
+    private int THREAD_NUM;
 
     private int startId;
 
-    private SampleCRUD(int startId) {
+    private SampleCRUD(int startId, int stepSize, int threadNum) {
+        this.STEP_SIZE = stepSize;
+        this.THREAD_NUM = threadNum;
         this.startId = startId;
-    }
-
-    private static void startInsert() throws InterruptedException {
-
-        ArrayBlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<Runnable>(50);
-        ThreadPoolExecutor pool = new ThreadPoolExecutor(50, 100, 1, TimeUnit.MINUTES, workQueue);
-        for (int i = 0; i < THREAD_NUM; i++) {
-            SampleCRUD thread = new SampleCRUD(alreadyDoneRecord + i * SampleCRUD.STEP_SIZE);
-            pool.execute(thread);
-        }
-        pool.shutdown();
-        while (!pool.isTerminated()) {
-            Thread.sleep(500);
-        }
     }
 
     public void run() {
@@ -56,12 +43,12 @@ public class SampleCRUD implements Runnable {
             for (int i = 0; i < STEP_SIZE; i++) {
                 //写入订单表，随机匹配商户和顾客id
                 //随机写入下单时间
-                Date randomDate = DateUtil.randomDate("2016-01-01 00:00:00", "2018-12-31 23:59:59");
+                Date randomDate = DateUtil.randomDate("2011-01-01 00:00:00", "2018-12-31 23:59:59");
                 try {
                     sb.append("(")
                             .append(startId + i).append(", ")
-                            .append(random.nextInt(10)).append(", ")
-                            .append(random.nextInt(100)).append(", ")
+                            .append(1 + random.nextInt(1000)).append(", ")
+                            .append(1 + random.nextInt(10000)).append(", ")
                             .append("0").append(", ")
                             .append("'").append(sd.format(randomDate)).append("', ")
                             .append("0, ")
@@ -74,16 +61,15 @@ public class SampleCRUD implements Runnable {
                     System.out.println(e.getMessage());
                 }
             }
+
             long startTime = System.currentTimeMillis();
             new QueryRunner(dataSource).insert(sb.substring(0, sb.length() - 1) + ";", new ResultSetHandler<Object[]>() {
                 public Object[] handle(ResultSet rs) {
                     return null;
                 }
             });
-            synchronized (SampleCRUD.class) {
-                alreadyDoneRecord += STEP_SIZE;
-            }
             System.out.println("insert " + (startId) + " ~ " + (startId + STEP_SIZE) + "(不含)  | " + STEP_SIZE + " BatchSize " + " 用时 " + (System.currentTimeMillis() - startTime) + "(ms)");
+
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (Exception e) {
@@ -94,14 +80,35 @@ public class SampleCRUD implements Runnable {
     public static void main(String[] args) {
         try {
             long startTime = System.currentTimeMillis();
-            int times = 1;
-            int stepPerLoop = SampleCRUD.STEP_SIZE * SampleCRUD.THREAD_NUM;
+            int times = 2;
             int index = 0;
+
+            int alreadyDoneRecord = 21000000;
+            final int STEP_SIZE = 50000;
+            final int THREAD_NUM = 20;
+
+            ArrayBlockingQueue<Runnable> workQueue;
+            ThreadPoolExecutor pool;
+
             while (index < times) {
                 index++;
-                SampleCRUD.startInsert();
+
+                workQueue = new ArrayBlockingQueue<Runnable>(30);
+                pool = new ThreadPoolExecutor(20, 100, 1, TimeUnit.MINUTES, workQueue);
+
+                for (int i = 0; i < THREAD_NUM; i++) {
+                    SampleCRUD thread = new SampleCRUD(alreadyDoneRecord + i * STEP_SIZE, STEP_SIZE, THREAD_NUM);
+                    pool.execute(thread);
+                }
+                pool.shutdown();
+                while (!pool.isTerminated()) {
+                    Thread.sleep(500);
+                }
+
+                System.out.println((index) + "_ 部分完成.");
+                alreadyDoneRecord += STEP_SIZE * THREAD_NUM;
             }
-            System.out.println("总共插入 " + stepPerLoop * times + " 用时 " + (System.currentTimeMillis() - startTime) + "(ms)");
+            System.out.println("总共插入 " + STEP_SIZE * THREAD_NUM * times + " 用时 " + (System.currentTimeMillis() - startTime) + "(ms)");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
